1cc6eff1fbd33de87582a073e538f53550c7d7be
MeetMRP <meetpatelmrp1005@gmail.com>
2023-12-15 07:59:00 +0530

    minor fixes

--- AI/hill_climbing.py
+++ AI/hill_climbing.py
@@ -1,5 +1,8 @@
 def get_neighbors(current, graph, n):
-    return [i for i in range(n) if i != current and graph[current][i] != None]
+    return [i 
+            for i in range(n) 
+            if i != current and graph[current][i] != None
+    ]
 
 def goal_test(current, goal):
     return current == goal
@@ -38,21 +41,21 @@ def hillclimb(start, goal, graph, n, mapping, h):
 
     print("\nPath not found")
 
-h = [14, 12, 11, 6, 4, 11, 0]
+h = [14, 12, 11, 6, 4]
+
+mapping = ['A', 'B', 'C', 'D', 'E']
+
 graph = [
-    [0, 4, 3, None, None, None, None],
-    [None, 0, None, None, 12, 5, None],
-    [None, None, 0, 7, 10, None, None],
-    [None, None, None, 0, 2, None, None],
-    [None, None, None, None, 0, None, 5],
-    [None, None, None, None, 0, 0, 16],
-    [None, None, None, None, None, 0, 0]
+    [0, 4, 10, None, None],
+    [None, 0, None, None, None],
+    [None, None, 0, 2, None],
+    [None, None, None, 0, 5],
+    [1, None, None, None, 0]
 ]
-mapping = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
 start = input("Enter Start point: ")
 goal = input("Enter Goal point: ")
 start = mapping.index(start)
 goal = mapping.index(goal)
 
-hillclimb(start, goal, graph, 7, mapping, h)
+hillclimb(start, goal, graph, len(mapping), mapping, h)
